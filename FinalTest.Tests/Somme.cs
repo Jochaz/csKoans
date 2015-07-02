5b925e6310a99eebc0a6c6943fbfb300c277a164
jochaz <jordan.chariot@gmail.com>
2015-07-02 09:19:08 +0200

    4eme test

---
+++ FinalTest.Tests/Somme.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FinalTest.Tests
+{
+    public class Somme
+    {
+        public bool PeutCalculer(string p0)
+        {
+            return p0.Substring(1, 1) == "+";
+        }
+
+        public int Calculer(string s)
+        {
+            return int.Parse(s.Substring(0, 1)) + int.Parse(s.Substring(2, 1)); ;
+        }
+    }
+}
