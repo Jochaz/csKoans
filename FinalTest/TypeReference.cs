1dd8a68de1e972c8a569286d9b004627b1e3b689
jochaz <jordan.chariot@gmail.com>
2015-07-02 08:58:40 +0200

    2ème test

---
+++ FinalTest/TypeReference.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace FinalTest
+{
+    public class TypeReference
+    {
+        private int _valeur;
+
+        public TypeReference(int i)
+        {
+            _valeur = i;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var unType = obj as TypeReference;
+
+            //Alt+Enter m'a créé ça !
+            return unType == null || unType._valeur == this._valeur;
+        }
+    }
+}
